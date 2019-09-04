87663de9b3bb241907ff42b3eeb3e098b8cc7a57
mohkam <mohkamfarid@gmail.com>
2019-09-05 00:59:43 +0500

    calculator

--- src/files/Screen.jsx
+++ src/files/Screen.jsx
@@ -20,6 +20,7 @@ class Screen extends React.Component {
       integer1: 0,
       integer2: null,
       operator: "",
+      mod_operator: "",
       int1_check: false,
       int2_check: false,
       int2_value_check: false,
@@ -37,6 +38,8 @@ class Screen extends React.Component {
       btn2: false,
       btn3: false,
       btn4: false,
+      int1_neg: false,
+      int2_neg: false,
       int2_dot_val_check: false,
       zero: "",
       active: "btn btn-secondary upper-btn cal-btn clr changeColor",
@@ -47,61 +50,49 @@ class Screen extends React.Component {
   set = (number) => {
     if (this.state.int2_check === false) {
       var check = this.state.integer1 / this.state.check_screen;
-      if (check <= 10) {
+      if (check <= 9) {
         console.log("in first condition");
         if (this.state.int1_dot === true) {
           const int1_dot_val = number / this.state.int1_divide;
-          const int1_dot_updated = this.state.integer1 + int1_dot_val;
+          // uncomment below for undo  ----------------------+++++++++++++++++++++++++++
+          // const int1_dot_updated = this.state.integer1 + int1_dot_val;
+          // console.log(int1_dot_updated);
 
-          console.log(int1_dot_updated);
+          // testing --------------------------------------------|||||||||||||||||||||||||||||||||||||||||||||
+          var int1_dot_updated = 0;
+          if (this.state.int1_neg === true) {
+            console.log("in true");
+            int1_dot_updated = this.state.integer1 - int1_dot_val;
+            console.log(int1_dot_updated);
+          } else {
+            int1_dot_updated = this.state.integer1 + int1_dot_val;
+          }
+          //testing end ---------------------------------------------||||||||||||||||||||||||||||||||||||||||||||||
           this.setState({
             integer1: int1_dot_updated,
-            screen: this.state.screen + number,
+            // screen: this.state.screen + number, <------------------- this is old line for undo
+            screen: int1_dot_updated, // <--------------- this is new line
             int1_check: true,
             int1_divide: this.state.int1_divide * 10
           });
-
-          // if (this.state.screen == "Error") {
-          //   console.log("in error");
-          //   this.setState({ screen: 0 }, () => {
-          //     console.log(this.state.screen);
-          //   });
-          // }
-
-          // ,
-          //   () => {
-          //     if (number === 0) {
-          //       console.log("in zero");
-          //       this.setState(
-          //         {
-          //           zero: this.state.zero + "0"
-          //         },
-          //         () => {
-          //           this.setState({
-          //             screen: this.state.screen + this.state.zero
-          //           });
-          //         }
-          //       );
-          //       console.log(this.state.screen);
-          //     }
-          //   }
         } else {
-          const int1Updated = this.state.integer1 * 10 + number;
-          this.setState(
-            {
-              integer1: int1Updated,
-              int1_check: true,
-              screen: int1Updated
-            }
-            // ,
-            // () => {
-            //   if (number === 0) {
-            //     console.log("in zero");
-            //     this.setState({ screen: this.state.zero + "0" });
-            //     console.log(this.state.screen);
-            //   }
-            // }
-          );
+          // uncomment below for undo
+          // const int1Updated = this.state.integer1 * 10 + number;
+
+          // testing -----------------------------------------||||||||||||||||||||
+          var int1Updated = 0;
+          if (this.state.int1_neg === true) {
+            int1Updated = this.state.integer1 * 10 - number;
+          } else {
+            int1Updated = this.state.integer1 * 10 + number;
+          }
+          // testing end --------------------------------|||||||||||||||||||||||||||||||||||||||||||||
+
+          this.setState({
+            integer1: int1Updated,
+            int1_check: true,
+            screen: int1Updated
+          });
         }
       } else {
         this.setState({ screen: this.state.integer1 });
@@ -111,16 +102,41 @@ class Screen extends React.Component {
       if (check <= 10) {
         if (this.state.int2_dot === true) {
           const int2_dot_val = number / this.state.int2_divide;
-          const int2_dot_updated = this.state.integer2 + int2_dot_val;
+          // uncomment below for undo  ----------------------+++++++++++++++++++++++++++
+          // const int2_dot_updated = this.state.integer2 + int2_dot_val;
+
+          // testing --------------------------------------------|||||||||||||||||||||||||||||||||||||||||||||
+          var int2_dot_updated = 0;
+          if (this.state.int2_neg === true) {
+            console.log("in true");
+            int2_dot_updated = this.state.integer2 - int2_dot_val;
+            console.log(int2_dot_updated);
+          } else {
+            int2_dot_updated = this.state.integer2 + int2_dot_val;
+          }
+          //testing end ---------------------------------------------||||||||||||||||||||||||||||||||||||||||||||||
+
           this.setState({
             integer2: int2_dot_updated,
-            screen: this.state.screen + number,
+            // screen: this.state.screen + number,  <------------------- this is old line for undo
+            screen: int2_dot_updated, // <--------------- this is new line
             int2_divide: this.state.int2_divide * 10,
             int2_value_check: true
           });
         } else {
           console.log("in second condition");
-          const int2Updated = this.state.integer2 * 10 + number;
+          // uncomment below for undo
+          // const int2Updated = this.state.integer2 * 10 + number;
+
+          // testing -----------------------------------------||||||||||||||||||||
+          var int2Updated = 0;
+          if (this.state.int2_neg === true) {
+            int2Updated = this.state.integer2 * 10 - number;
+          } else {
+            int2Updated = this.state.integer2 * 10 + number;
+          }
+          // testing end --------------------------------|||||||||||||||||||||||||||||||||||||||||||||
+
           this.setState({
             integer2: int2Updated,
             screen: int2Updated,
@@ -142,7 +158,7 @@ class Screen extends React.Component {
       this.colorChange(oprt);
       this.setState(
         {
-          operator: oprt
+          mod_operator: oprt
         },
         () => {
           this.calculate();
@@ -189,77 +205,91 @@ class Screen extends React.Component {
       this.setState({ screen: 0 });
     } else if (
       this.state.int2_value_check === false &&
-      this.state.operator !== ""
+      (this.state.operator !== "" || this.state.mod_operator !== "")
     ) {
-      switch (this.state.operator) {
-        case "%":
-          var result_local = this.state.integer1 / 100;
-          console.log("in %");
-          this.setState({
-            screen: result_local,
-            integer1: result_local,
-            again_operator: this.state.operator,
-            again_cal_int2: this.state.integer2,
-            result: [...this.state.result, result_local],
-            integer2: 0,
-            operator: ""
-          });
-          break;
-        case "+":
-          var result_local = this.state.integer1 + this.state.again_int1;
-          this.setState({
-            screen: result_local,
-            integer1: result_local,
-            again_operator: this.state.operator,
-            result: [...this.state.result, result_local],
+      if (this.state.mod_operator === "%") {
+        var result_local = this.state.integer1 / 100;
+        console.log("in % ....");
+        this.setState({
+          screen: result_local,
+          integer1: result_local,
+          // again_operator: this.state.operator,
+          // again_cal_int2: this.state.integer2,
+          result: [...this.state.result, result_local],
+          integer2: 0
+        });
+      } else {
+        switch (this.state.operator) {
+          case "%":
+            var result_local = this.state.integer1 / 100;
+            console.log("in %");
+            this.setState({
+              screen: result_local,
+              integer1: result_local,
+              again_operator: this.state.operator,
+              again_cal_int2: this.state.integer2,
+              result: [...this.state.result, result_local],
+              integer2: 0
+              // ,
+              // operator: ""
+            });
+            break;
+          case "+":
+            var result_local = this.state.integer1 + this.state.again_int1;
+            this.setState({
+              screen: result_local,
+              integer1: result_local,
+              again_operator: this.state.operator,
+              result: [...this.state.result, result_local],
 
-            integer2: 0,
-            int2_check: false
-          });
-          break;
-        case "/":
-          var result_local = this.state.integer1 / this.state.again_int1;
+              integer2: 0,
+              int2_check: false
+            });
+            break;
+          case "/":
+            var result_local = this.state.integer1 / this.state.again_int1;
 
-          this.setState({
-            screen: result_local,
-            integer1: result_local,
-            again_operator: this.state.operator,
-            result: [...this.state.result, result_local],
+            this.setState({
+              screen: result_local,
+              integer1: result_local,
+              again_operator: this.state.operator,
+              result: [...this.state.result, result_local],
 
-            integer2: 0,
-            int2_check: false
-          });
-          break;
-        case "*":
-          var result_local = this.state.integer1 * this.state.again_int1;
-          this.setState({
-            screen: result_local,
-            integer1: result_local,
-            again_operator: this.state.operator,
-            result: [...this.state.result, result_local],
+              integer2: 0,
+              int2_check: false
+            });
+            break;
+          case "*":
+            var result_local = this.state.integer1 * this.state.again_int1;
+            this.setState({
+              screen: result_local,
+              integer1: result_local,
+              again_operator: this.state.operator,
+              result: [...this.state.result, result_local],
 
-            integer2: 0,
-            int2_check: false
-          });
-          break;
-        case "-":
-          var result_local = this.state.integer1 - this.state.again_int1;
-          this.setState({
-            screen: result_local,
-            integer1: result_local,
-            again_operator: this.state.operator,
-            result: [...this.state.result, result_local],
+              integer2: 0,
+              int2_check: false
+            });
+            break;
+          case "-":
+            var result_local = this.state.integer1 - this.state.again_int1;
+            this.setState({
+              screen: result_local,
+              integer1: result_local,
+              again_operator: this.state.operator,
+              result: [...this.state.result, result_local],
 
-            integer2: 0,
-            int2_check: false
-          });
-          break;
+              integer2: 0,
+              int2_check: false
+            });
+            break;
+        }
       }
     } else if (
       this.state.int2_value_check === true &&
       this.state.operator === ""
     ) {
-      console.log("in again");
+      console.log("in again calculation");
       switch (this.state.again_operator) {
         case "+":
           var result_local = this.state.integer1 + this.state.again_cal_int2;
@@ -302,7 +332,7 @@ class Screen extends React.Component {
       switch (this.state.operator) {
         case "%":
           var result_local = this.state.integer1 / 100;
-          console.log("in condition");
+          console.log("in condition of %");
           this.setState({
             screen: result_local,
             integer1: result_local,
@@ -415,12 +445,25 @@ class Screen extends React.Component {
     }
   };
   negate = () => {
-    if (this.state.int1_check === true) {
+    if (this.state.int2_check === false) {
+      console.log("in neg");
       const neg = this.state.integer1 * -1;
-      this.setState({ integer1: neg, screen: neg });
+      this.setState({
+        int1_neg: !this.state.int1_neg,
+        screen: neg,
+        integer1: neg
+      });
+
+      // this.setState({ integer1: neg, screen: neg });
     } else {
       const neg = this.state.integer2 * -1;
-      this.setState({ integer2: neg, screen: neg });
+      this.setState({
+        int2_neg: !this.state.int2_neg,
+        screen: neg,
+        integer2: neg
+      });
+      // const neg = this.state.integer2 * -1;
+      // this.setState({ integer2: neg, screen: neg });
     }
   };
   show = () => {
@@ -435,6 +478,7 @@ class Screen extends React.Component {
       integer1: 0,
       integer2: null,
       operator: "",
+      mod_operator: "",
       int1_check: false,
       int2_check: false,
       screen: 0,
@@ -449,6 +493,8 @@ class Screen extends React.Component {
       btn3: false,
       btn4: false,
       int2_dot_val_check: false,
+      int1_neg: false,
+      int2_neg: false,
       zero: "",
       int1_divide: 10,
       int2_divide: 10
@@ -458,7 +504,7 @@ class Screen extends React.Component {
       "output screen for errors or memory";
   };
   colorChange = (oprt) => {
-    console.log("in change");
+    console.log("in change color");
     if (oprt === "/") {
       this.setState({ btn1: true, btn2: false, btn3: false, btn4: false });
     } else if (oprt === "*") {
@@ -469,9 +515,7 @@ class Screen extends React.Component {
       this.setState({ btn1: false, btn2: false, btn3: false, btn4: true });
     }
   };
-  // componentDidUpdate() {
-  //   <Result show={this.state.screen} />;
-  // }
+
   render() {
     return (
       <React.Fragment>
